2704b1a3d50725006d4f0ee1e5d68c5c6f6e4286
XTao <xutao881001@gmail.com>
2013-01-03 15:46:32 +0800

    fix pid init

--- BugBot/BugBot.ino
+++ BugBot/BugBot.ino
@@ -63,7 +63,9 @@ struct pid_t {
 pid_t pid = {
   PID_P_FACTOR,
   PID_I_FACTOR,
-  PID_D_FACTOR
+  PID_D_FACTOR,
+  0,
+  0
 };
 
 
@@ -180,16 +182,17 @@ int driveServo()
 int processPID(int setPoint, int processValue, struct pid_t *pid)
 {
     int error = 0;
-    int p_term;
-    int i_term;
-    int d_term;
-    int ret;
+    int p_term = 0;
+    int i_term = 0;
+    int d_term = 0;
+    int ret = 0;
     error = setPoint - processValue;
     p_term = pid->p_factor * error;
-    pid->sum_error = pid->sum_error + error;
-    i_term = pid->i_factor * pid->sum_error;
+    //pid->sum_error = pid->sum_error + error;
+    //i_term = pid->i_factor * pid->sum_error;
     d_term = pid->d_factor * (pid->last_error - error);
     pid->last_error = error;
     ret = p_term + i_term + d_term;
+    /* TODO: check value range */
     return ret;
 }
