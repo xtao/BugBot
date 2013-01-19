4479e04ca4e4651f731220a0b2ae653145d9cbf2
XTao <xutao881001@gmail.com>
2013-01-19 17:38:13 +0800

    MOD: PID optimization 6 @ 2013-01-19

--- CarBot/CarBot.ino
+++ CarBot/CarBot.ino
@@ -29,9 +29,9 @@
 #define MOTOR_LEFT 0
 #define MOTOR_RIGHT 1
 
-#define PID_P_FACTOR 4
+#define PID_P_FACTOR 4.1
 #define PID_I_FACTOR 0
-#define PID_D_FACTOR 2
+#define PID_D_FACTOR 5
 #define PID_SCALING_FACTOR 128
 
 struct pid_t {
