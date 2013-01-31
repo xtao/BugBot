04347143ed2b95bf1539c3d34a2a9e237191d73c
XTao <xutao881001@gmail.com>
2013-01-31 16:19:43 +0800

    MOD: output the IR info to serial

--- TestIR/TestIR.ino
+++ TestIR/TestIR.ino
@@ -4,9 +4,20 @@
  Copyright @ InfiniteSense.com
  */
 
-#define IR_INPUT_PIN A0
-#define IR_CONTROL_PIN 4
+/* ir pin */
+#define PIN_IR1_LEFT A0
+#define PIN_IR2_LEFT A1
+#define PIN_IR3_LEFT A2
+#define PIN_IR3_RIGHT A3
+#define PIN_IR2_RIGHT A4
+#define PIN_IR1_RIGHT A5
 
+#define IR_NUM 6
+
+int ir_array[IR_NUM] = { 0 };
+const int ir_pin_array[IR_NUM] = {
+   PIN_IR1_LEFT, PIN_IR2_LEFT, PIN_IR3_LEFT, PIN_IR3_RIGHT, PIN_IR2_RIGHT, PIN_IR1_RIGHT
+};
 
 void setup()
 {
@@ -17,28 +28,26 @@ void setup()
 void loop()
 {
   readIR();
-  delay(500);
+  delay(1000);
 }
 
 void initIR() {
-  pinMode(IR_INPUT_PIN, INPUT);  
-  pinMode(IR_CONTROL_PIN, OUTPUT);
-  digitalWrite(IR_CONTROL_PIN, HIGH);
+  int i;
+  // setting input pins
+  for (i = 0; i < IR_NUM; i++) {
+    pinMode(ir_pin_array[i], INPUT);
+  }
 }
 
 void readIR() {
-  int ir_diff;
-  int ir_temp;
-  ir_diff = analogRead(IR_INPUT_PIN);
-  Serial.print(ir_diff);
-  digitalWrite(IR_CONTROL_PIN, LOW);
-  delay(500);
-  Serial.print(" ");
-  ir_temp = analogRead(IR_INPUT_PIN);
-  Serial.print(ir_temp);
-  Serial.print(" ");
-  ir_diff = ir_temp - ir_diff;
-  Serial.print(ir_diff);
-  digitalWrite(IR_CONTROL_PIN, HIGH);
+  int i;
+  for (i = 0; i < IR_NUM; i++) {
+     ir_array[i] = digitalRead(ir_pin_array[i]);
+     if (ir_array[i] == LOW) {
+       Serial.print("0 ");
+     } else {
+       Serial.print("1 ");
+     }
+  }
   Serial.print("\n");
 }
